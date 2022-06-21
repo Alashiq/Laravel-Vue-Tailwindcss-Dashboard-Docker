090b0bd2a438078196c970cc5f9a2e8184f6902c
Abdulsamia Alashiq <alashiq@Abdulsamias-MacBook-Pro.local>
2022-06-21 10:59:08 +0200

    create seed for role table and admin table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,21 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        DB::table('permissions')->insert([
+            'name' => 'Test User',
+            'permissions' => '["HomeChart","ReadMessage"]',
+            // 'password' => Hash::make('password'),
+        ]);
+
+
+        DB::table('admins')->insert([
+            'phone' => '0926503011',
+            'first_name' => 'Abdulsmaia',
+            'last_name' => 'Alashiq',
+            'role_id' => 1,
+            'password' => Hash::make('123123'),
+            'state' => 1,
+        ]);
     }
 }
